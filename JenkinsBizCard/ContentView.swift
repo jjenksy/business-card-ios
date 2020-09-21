f7df57079b0d240fa6410fb3865c48e8cf730e64
jenksy <jjenksy@gmail.com>
2020-09-21 08:25:13 -0500

    Initial Commit

---
+++ JenkinsBizCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JenkinsBizCard
+//
+//  Created by John Jenkins on 9/21/20.
+//  Copyright © 2020 John Jenkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
